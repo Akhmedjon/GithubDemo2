e2c4f2b29f8754b10c66642bd08c02e1bf4d7234
Ahmedjon <boltaevahmedjon28@gmail.com>
2023-02-14 22:44:26 +0500

    Initial Commit

---
+++ GithubDemo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubDemo2
+//
+//  Created by Ахмеджон Болтаев on 14/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Welcome to SwiftUI")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Ахмеджон Болтаев on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
